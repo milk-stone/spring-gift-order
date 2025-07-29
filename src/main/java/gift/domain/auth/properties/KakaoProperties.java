81f37846ca440715caa8574861b93026d47956ae
milk-stone <ywshero@naver.com>
2025-07-29 12:24:54 +0900

    feat : application.properties 의 client-id, redirect-uri 값을 안전하게 받아올 수 있는 KakaoProperties 클래스 작성

---
+++ src/main/java/gift/domain/auth/properties/KakaoProperties.java
@@ -0,0 +1,27 @@
+package gift.domain.auth.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "kakao")
+public class KakaoProperties {
+    private String clientId;
+    private String redirectUri;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+}
