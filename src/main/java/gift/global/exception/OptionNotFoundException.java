38342bc9741ba72409affd21fef26c8aa2b6313c
milk-stone <ywshero@naver.com>
2025-08-01 17:19:54 +0900

    feat : OptionNotFoundException 예외 처리 클래스 작성 및 전역 예외처리 핸들러 설정

---
+++ src/main/java/gift/global/exception/OptionNotFoundException.java
@@ -0,0 +1,7 @@
+package gift.global.exception;
+
+public class OptionNotFoundException extends RuntimeException {
+    public OptionNotFoundException(String message) {
+        super(message);
+    }
+}
