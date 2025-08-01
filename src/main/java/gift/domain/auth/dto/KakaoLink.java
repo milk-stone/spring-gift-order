340f6ac1ba54bb09d5e9dcc2457fd4ee2fa3050c
milk-stone <ywshero@naver.com>
2025-08-01 17:22:17 +0900

    feat : 주문 요청, 주문 요청 결과 반환, 카카오톡 요청, 카카오톡 송신 결과 반환 DTO 작성

---
+++ src/main/java/gift/domain/auth/dto/KakaoLink.java
@@ -0,0 +1,12 @@
+package gift.domain.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record KakaoLink(
+        @JsonProperty("web_url")
+        String webUrl,
+
+        @JsonProperty("mobile_web_url")
+        String mobileWebUrl
+) {
+}
